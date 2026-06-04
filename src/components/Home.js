d1721e5cee73873530a0e0b94a3fc94c3eadc106
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-06-04 12:05:44 -0400

    Didn't get into Mechatronics

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,7 +33,7 @@ const Home = () => {
         <div className="home-content">
           <div className="home-text fade-in-up">
             <h1 className="home-title">AURICK ANWAR</h1>
-            <h3 className="home-subtitle">Mechatronics Engineering @McMaster University</h3>
+            <h3 className="home-subtitle">Engineering Physics @McMaster University</h3>
             <div className="home-role-lines">
               <p>
                 Founding Engineer @{''}
