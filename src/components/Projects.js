045a6e2bf03dd1506a69ab0983f6be0a224521a0
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-07-26 00:26:32 -0400

    Update Progress on TeleARM

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
   const warehouseCleanup = {
     title: 'TeleARM',
     status: 'Currently Building',
-    progressPercent: 25,
+    progressPercent: 30,
     description:
       'Using a Depth Camera, the robot is driven autonomously and picks up items when an object is detected!',
     bullets: [
